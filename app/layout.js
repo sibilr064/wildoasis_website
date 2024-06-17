4a20883ded2b0c31c7c309a55b86e858ab3872ac
sibilr064 <sibilr064@gmail.com>
2024-06-17 19:13:14 +0530

    font optimized

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/navigation";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -15,7 +21,9 @@ import "@/app/_styles/globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
