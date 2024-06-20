00bb840c16e7d1ddaa77ed30ae44827fd2acc9e1
sibilr064 <sibilr064@gmail.com>
2024-06-20 12:36:31 +0530

    Improved navigation and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/navigation";
 import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({
@@ -17,19 +15,18 @@ export const metadata = {
 };
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 bg-blue-700 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-800">{children}</main>
+        </div>
       </body>
     </html>
   );
